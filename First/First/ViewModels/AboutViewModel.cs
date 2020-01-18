a969695eff47b393cc171652c11136dffaf89a04
BuildTools <unconfigured@null.spigotmc.org>
2020-01-18 10:27:42 -0600

    Add project files.

---
+++ First/First/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace First.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+        }
+        public string AboutMe { get; set; } = "Hi! My name is Adam and this is my first project!";
+        public ICommand OpenWebCommand { get; }
+    }
+}
