240409b424e6ec792884af309a1a9530baa9afd0
BuildTools <unconfigured@null.spigotmc.org>
2020-01-18 10:54:49 -0600

    added wpf app

---
+++ First.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace First.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public int NumberOfTimes { get; private set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NumberOfTimes = NumberOfTimes + 1;
+            Button1.Content = "You clicked me " + NumberOfTimes;
+        }
+    }
+}
